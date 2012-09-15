0d1c41b7a244b37fcd794ef4e6d300739b4b2757
Herryanto Siatono <herryanto@gmail.com>
2012-09-15 23:14:54 +0800

    Fix rows overlap issue

--- sprintboard-booster.js
+++ sprintboard-booster.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       Sprintboard Booster
 // @namespace  http://siatono.com/
-// @version    0.3
+// @version    0.3.1
 // @description Sprintboard booster
 // @match      http://ci.media.corp.yahoo.com:9999/sprintboard
 // @require    http://yui.yahooapis.com/combo?3.6.0/yui/yui-min.js&3.6.0/attribute-core/attribute-core-min.js&3.6.0/oop/oop-min.js&3.6.0/event-custom-base/event-custom-base-min.js&3.6.0/event-custom-complex/event-custom-complex-min.js&3.6.0/attribute-events/attribute-events-min.js&3.6.0/attribute-extras/attribute-extras-min.js&3.6.0/attribute-base/attribute-base-min.js&3.6.0/attribute-complex/attribute-complex-min.js&3.6.0/base-core/base-core-min.js&3.6.0/base-base/base-base-min.js&3.6.0/pluginhost-base/pluginhost-base-min.js&3.6.0/pluginhost-config/pluginhost-config-min.js&3.6.0/base-pluginhost/base-pluginhost-min.js&3.6.0/classnamemanager/classnamemanager-min.js&3.6.0/dom-core/dom-core-min.js&3.6.0/dom-base/dom-base-min.js&3.6.0/selector-native/selector-native-min.js&3.6.0/selector/selector-min.js&3.6.0/node-core/node-core-min.js&3.6.0/node-base/node-base-min.js&3.6.0/event-base/event-base-min.js&3.6.0/event-synthetic/event-synthetic-min.js&3.6.0/event-focus/event-focus-min.js&3.6.0/dom-style/dom-style-min.js&&3.6.0/build/node-screen/node-screen-min.js&3.6.0/build/node-style/node-style-min.js
@@ -14,13 +14,17 @@ YUI().use('base', 'node', function(Y) {
     }
     
     Y.extend(SprintboardBooster, Y.Base, {
+        
         initializer: function() {
+            console.log("initialize");
             this._turnOn();
             this._bindUI();
         },
         
         _turnOn: function() {
             var isOn = this._isOn;
+            
+            console.log("turning on");
             
             if (!this._isSprintboardLoaded()) {
                 return Y.later(1000, this, this._turnOn);
@@ -31,7 +35,7 @@ YUI().use('base', 'node', function(Y) {
             }
                         
             this._loadUserAvatars();
-            this._updateBoardRowsHeight();
+            this._updateRowHeights();
             this._isOn = true;
         },
         
@@ -84,36 +88,16 @@ YUI().use('base', 'node', function(Y) {
             this._isOn = true;
         },
 
-        /** 
-         * Rows height is reset by existing script, sticky node height is changed, thus the resize is needed.
-         */
-        _updateBoardRowsHeight: function() {
+        _updateRowHeights: function() {
             var rowNodes = Y.Node.all('#content .board_row'),
                 boardNodes, stickyNodes, rowHeight;
             
-            rowNodes.each(function(rowNode) {
-                rowHeight = 0;
-
-                // Get tallest board
-                tallestBoardCount = 0;
-                tallestBoardIndex = 0;
-                boardNodes = rowNode.all('.board');
-                boardNodes.each(function(boardNode, i) {
-                    stickyNodes = boardNode.all('.sticky');
-                    
-                    if (stickyNodes.size() > tallestBoardCount) {
-                        tallestBoardIndex = i;
-                        tallestBoardCount = stickyNodes.size();
-                    }                    
-                });
-                
-                // Calculate tallest board height
-                stickyNodes = boardNodes.item(tallestBoardIndex).all('.sticky');
-                stickyNodes.each(function(stickyNode) {
-                    rowHeight += stickyNode.get('offsetHeight') + 10;
-                });
-                                
-                rowNode.setStyle('height', (rowHeight + 10 + 'px'));
+            Y.all('div.yui3-g').each(function(node) {
+                node.setStyle('height', null);
+            });
+            
+            Y.all('div.yui3-g').each(function(node) {
+                node.setStyle('height', node.get('offsetHeight') + 'px');
             });
         },
         
@@ -128,6 +112,9 @@ YUI().use('base', 'node', function(Y) {
                 rowNodes = Y.Node.all('#content .board_row'),
                 currentUsername = focusSelectorNode.get('value');
             
+            console.log('selectetorNode', focusSelectorNode);
+            console.log('value:', currentUsername);
+                
             if (currentUsername === 'none') {
                 rowNodes.setStyle('display', 'block');
             } else {
@@ -148,7 +135,10 @@ YUI().use('base', 'node', function(Y) {
         _userBoards: []
     });
     
+    console.log(Y.later);
+    
     Y.later(3000, this, function() {
+        console.log("later");
         new SprintboardBooster();
     });
 });
